e5ca31c85d2a9fab56add7badde3f8fc170129f5
unknown <umaryounas14@gmail.com>
2021-04-01 12:43:37 +0500

    Fixed heroku post build bug

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const path = require('path')
 const errorMiddleware = require('./middlewares/errors')
 
 // Setting up config file 
-if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
+if (process.env.NODE_ENV === 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 // dotenv.config({ path: 'backend/config/config.env' })
 
 app.use(express.json());
--- backend/config/database.js
+++ backend/config/database.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI,{
+    mongoose.connect(process.env.DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true,
+        useCreateIndex: true
     }).then(con => {
-        console.log(`MongoDB Database connected with Host: ${con.connection.host}`)
+        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
     })
 }
 
